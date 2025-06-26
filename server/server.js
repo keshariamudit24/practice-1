a4cf226afc4d1b69e2618efe3653b22b437ecd0d
keshariamudit24 <keshariamudit@gmail.com>
2025-06-26 16:23:45 +0530

    server.js changes

--- server/server.js
+++ server/server.js
@@ -1,1 +1,14 @@
 // this is server.js
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const app = express();
+const port = process.env.PORT
+
+app.use(express.json());
+
+mongoose.connect(process.env.MONGODB_URL)
+    .then(() => app.listen(port, () => { console.log(`server running on port ${port}`) }))
+    .catch()
+
+// routes 
